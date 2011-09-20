78f00159c4c71815ce4f2453e6cf80046b12ebe7
wishcraft <wishcraft@users.noreply.github.com>
2011-09-20 07:25:11 +0000

    Benchmark 1.05 Working Release - RC Soon just have to write a report.

--- releases/1.05/htdocs/modules/benchmark/sql/mysql.sql
+++ releases/1.05/htdocs/modules/benchmark/sql/mysql.sql
@@ -12,33 +12,33 @@ CREATE TABLE `benchmark_fields` (
   `recipricol` int(5) DEFAULT NULL,
   `alter_type` enum('bigint','mediumint','smallint','int','tinyint','float','decimal','double','enum_varchar','enum_int','varchar','mediumtext','text','date','time','datetime','year') DEFAULT NULL,
   `created` int(13) unsigned DEFAULT '0',
-  `updated` int(13) unsigned DEFAULT '0',  
+  `updated` int(13) unsigned DEFAULT '0',
   `actioned` int(13) unsigned DEFAULT '0',
   PRIMARY KEY (`fid`),
-  KEY `common` (`fid`,`type`,`required`,`primarykey`,`alter_type`, `key`(10))
+  KEY `common` (`fid`,`type`,`required`,`primarykey`,`alter_type`,`key`(10))
 ) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8;
 
-insert  into `benchmark_fields` (`fid`,`name`,`type`,`required`,`primarykey`,`key`,`create_sql`,`alter_sql`,`size`,`recipricol`,`alter_type`,`created`) values (1,'id','int','_YES','_YES','common','INT(20) UNSIGNED NOT NULL AUTO_INCREMENT','INT(10) UNSIGNED NOT NULL AUTO_INCREMENT',20,0,'int', UNIX_TIMESTAMP());
-insert  into `benchmark_fields` (`fid`,`name`,`type`,`required`,`primarykey`,`key`,`create_sql`,`alter_sql`,`size`,`recipricol`,`alter_type`,`created`) values (2,'tid','int','_YES','_NO','common','INT(10) UNSIGNED DEFAULT \'0\'','INT(20) UNSIGNED DEFAULT \'0\'',10,0,'int', UNIX_TIMESTAMP());
-insert  into `benchmark_fields` (`fid`,`name`,`type`,`required`,`primarykey`,`key`,`create_sql`,`alter_sql`,`size`,`recipricol`,`alter_type`,`created`) values (3,'boolean','tinyint','_NO','_NO',NULL,'TINYINT(1) DEFAULT NULL','INT(1) DEFAULT NULL',1,0,'int', UNIX_TIMESTAMP());
-insert  into `benchmark_fields` (`fid`,`name`,`type`,`required`,`primarykey`,`key`,`create_sql`,`alter_sql`,`size`,`recipricol`,`alter_type`,`created`) values (4,'bigint','bigint','_NO','_NO',NULL,'BIGINT(20) DEFAULT \'0\'','INT(35) DEFAULT \'0\'',20,0,'int', UNIX_TIMESTAMP());
-insert  into `benchmark_fields` (`fid`,`name`,`type`,`required`,`primarykey`,`key`,`create_sql`,`alter_sql`,`size`,`recipricol`,`alter_type`,`created`) values (5,'mediumint','mediumint','_NO','_NO',NULL,'MEDIUMINT(20) DEFAULT \'0\'','BIGINT(15) DEFAULT \'0\'',20,0,'bigint', UNIX_TIMESTAMP());
-insert  into `benchmark_fields` (`fid`,`name`,`type`,`required`,`primarykey`,`key`,`create_sql`,`alter_sql`,`size`,`recipricol`,`alter_type`,`created`) values (6,'smallint','smallint','_NO','_NO',NULL,'SMALLINT(20) DEFAULT \'0\'','INT(15) DEFAULT \'0\'',20,0,'int', UNIX_TIMESTAMP());
-insert  into `benchmark_fields` (`fid`,`name`,`type`,`required`,`primarykey`,`key`,`create_sql`,`alter_sql`,`size`,`recipricol`,`alter_type`,`created`) values (7,'int','int','_NO','_NO',NULL,'INT(20) DEFAULT \'0\'','MEDIUMINT(16) DEFAULT \'0\'',20,0,'mediumint', UNIX_TIMESTAMP());
-insert  into `benchmark_fields` (`fid`,`name`,`type`,`required`,`primarykey`,`key`,`create_sql`,`alter_sql`,`size`,`recipricol`,`alter_type`,`created`) values (8,'tinyiny','tinyint','_NO','_NO',NULL,'TINYINT(20) DEFAULT \'0\'','SMALLINT(14) DEFAULT \'0\'',20,0,'smallint', UNIX_TIMESTAMP());
-insert  into `benchmark_fields` (`fid`,`name`,`type`,`required`,`primarykey`,`key`,`create_sql`,`alter_sql`,`size`,`recipricol`,`alter_type`,`created`) values (9,'float','float','_NO','_NO',NULL,'FLOAT(10,5) DEFAULT \'0.00000\'','DOUBLE(16,8) DEFAULT \'0.000000000\'',10,5,'double', UNIX_TIMESTAMP());
-insert  into `benchmark_fields` (`fid`,`name`,`type`,`required`,`primarykey`,`key`,`create_sql`,`alter_sql`,`size`,`recipricol`,`alter_type`,`created`) values (10,'decimal','decimal','_NO','_NO',NULL,'DECIMAL(10,5) DEFAULT \'0.00000\'','FLOAT(16,8) DEFAULT \'0.00000000\'',10,5,'float', UNIX_TIMESTAMP());
-insert  into `benchmark_fields` (`fid`,`name`,`type`,`required`,`primarykey`,`key`,`create_sql`,`alter_sql`,`size`,`recipricol`,`alter_type`,`created`) values (11,'double','double','_NO','_NO',NULL,'DOUBLE(10,5) DEFAULT \'0.00000\'','DECIMAL(16,8) DEFAULT \'0.00000000\'',10,5,'decimal', UNIX_TIMESTAMP());
-insert  into `benchmark_fields` (`fid`,`name`,`type`,`required`,`primarykey`,`key`,`create_sql`,`alter_sql`,`size`,`recipricol`,`alter_type`,`created`) values (12,'enum_int','enum_int','_NO','_NO',NULL,'ENUM(%numbersassymbols%) DEFAULT NULL','SMALLINT(5) DEFAULT NULL',2,0,'smallint', UNIX_TIMESTAMP());
-insert  into `benchmark_fields` (`fid`,`name`,`type`,`required`,`primarykey`,`key`,`create_sql`,`alter_sql`,`size`,`recipricol`,`alter_type`,`created`) values (13,'enum_varchar','enum_varchar','_NO','_NO',NULL,'ENUM(%numbersaswords%) DEFAULT NULL','VARCHAR(35) DEFAULT NULL',15,0,'varchar', UNIX_TIMESTAMP());
-insert  into `benchmark_fields` (`fid`,`name`,`type`,`required`,`primarykey`,`key`,`create_sql`,`alter_sql`,`size`,`recipricol`,`alter_type`,`created`) values (14,'mediumtext','mediumtext','_NO','_NO',NULL,'MEDIUMTEXT','VARCHAR(2000) DEFAULT NULL',2000,0,'varchar', UNIX_TIMESTAMP());
-insert  into `benchmark_fields` (`fid`,`name`,`type`,`required`,`primarykey`,`key`,`create_sql`,`alter_sql`,`size`,`recipricol`,`alter_type`,`created`) values (15,'text','text','_NO','_NO',NULL,'TEXT','VARCHAR(2000) DEFAULT NULL',2000,0,'varchar', UNIX_TIMESTAMP());
-insert  into `benchmark_fields` (`fid`,`name`,`type`,`required`,`primarykey`,`key`,`create_sql`,`alter_sql`,`size`,`recipricol`,`alter_type`,`created`) values (16,'date','date','_NO','_NO',NULL,'DATE DEFAULT NULL','VARCHAR(12) DEFAULT NULL',12,0,'varchar', UNIX_TIMESTAMP());
-insert  into `benchmark_fields` (`fid`,`name`,`type`,`required`,`primarykey`,`key`,`create_sql`,`alter_sql`,`size`,`recipricol`,`alter_type`,`created`) values (17,'time','time','_NO','_NO',NULL,'TIME DEFAULT NULL','VARCHAR(6) DEFAULT NULL',6,0,'varchar', UNIX_TIMESTAMP());
-insert  into `benchmark_fields` (`fid`,`name`,`type`,`required`,`primarykey`,`key`,`create_sql`,`alter_sql`,`size`,`recipricol`,`alter_type`,`created`) values (18,'datetime','datetime','_NO','_NO',NULL,'DATETIME DEFAULT NULL','VARCHAR(20) DEFAULT NULL',20,0,'varchar', UNIX_TIMESTAMP());
-insert  into `benchmark_fields` (`fid`,`name`,`type`,`required`,`primarykey`,`key`,`create_sql`,`alter_sql`,`size`,`recipricol`,`alter_type`,`created`) values (19,'varchar','varchar','_NO','_NO',NULL,'VARCHAR(255) DEFAULT NULL','MEDIUMTEXT',255,0,'mediumtext', UNIX_TIMESTAMP());
-insert  into `benchmark_fields` (`fid`,`name`,`type`,`required`,`primarykey`,`key`,`create_sql`,`alter_sql`,`size`,`recipricol`,`alter_type`,`created`) values (20,'created','int','_YES','_NO','made','INT(12) DEFAULT \'0\'','MEDIUMINT(12) DEFAULT \'0\'',12,0,'mediumint', UNIX_TIMESTAMP());
-insert  into `benchmark_fields` (`fid`,`name`,`type`,`required`,`primarykey`,`key`,`create_sql`,`alter_sql`,`size`,`recipricol`,`alter_type`,`created`) values (21,'year','year','_NO','_NO',NULL,'YEAR DEFAULT NULL','VARCHAR(4) DEFAULT NULL',4,0,'varchar', UNIX_TIMESTAMP());
+insert  into `benchmark_fields` (`fid`,`name`,`type`,`required`,`primarykey`,`key`,`create_sql`,`alter_sql`,`size`,`recipricol`,`alter_type`,`created`,`updated`,`actioned`) values (1,'id','int','_YES','_YES','common','INT(20) UNSIGNED NOT NULL AUTO_INCREMENT','INT(10) UNSIGNED NOT NULL AUTO_INCREMENT',20,0,'int',1316464298,0,0);
+insert  into `benchmark_fields` (`fid`,`name`,`type`,`required`,`primarykey`,`key`,`create_sql`,`alter_sql`,`size`,`recipricol`,`alter_type`,`created`,`updated`,`actioned`) values (2,'tid','int','_YES','_NO','common','INT(10) UNSIGNED DEFAULT NULL','INT(20) UNSIGNED DEFAULT NULL',10,0,'int',1316464299,0,0);
+insert  into `benchmark_fields` (`fid`,`name`,`type`,`required`,`primarykey`,`key`,`create_sql`,`alter_sql`,`size`,`recipricol`,`alter_type`,`created`,`updated`,`actioned`) values (3,'boolean','tinyint','_NO','_NO',NULL,'TINYINT(1) DEFAULT NULL','INT(1) DEFAULT NULL',1,0,'int',1316464299,0,0);
+insert  into `benchmark_fields` (`fid`,`name`,`type`,`required`,`primarykey`,`key`,`create_sql`,`alter_sql`,`size`,`recipricol`,`alter_type`,`created`,`updated`,`actioned`) values (4,'bigint','bigint','_NO','_NO',NULL,'BIGINT(20) DEFAULT NULL','INT(35) DEFAULT NULL',20,0,'int',1316464299,0,0);
+insert  into `benchmark_fields` (`fid`,`name`,`type`,`required`,`primarykey`,`key`,`create_sql`,`alter_sql`,`size`,`recipricol`,`alter_type`,`created`,`updated`,`actioned`) values (5,'mediumint','mediumint','_NO','_NO',NULL,'MEDIUMINT(20) DEFAULT NULL','BIGINT(15) DEFAULT NULL',20,0,'bigint',1316464299,0,0);
+insert  into `benchmark_fields` (`fid`,`name`,`type`,`required`,`primarykey`,`key`,`create_sql`,`alter_sql`,`size`,`recipricol`,`alter_type`,`created`,`updated`,`actioned`) values (6,'smallint','smallint','_NO','_NO',NULL,'SMALLINT(20) DEFAULT NULL','INT(15) DEFAULT NULL',20,0,'int',1316464299,0,0);
+insert  into `benchmark_fields` (`fid`,`name`,`type`,`required`,`primarykey`,`key`,`create_sql`,`alter_sql`,`size`,`recipricol`,`alter_type`,`created`,`updated`,`actioned`) values (7,'int','int','_NO','_NO',NULL,'INT(20) DEFAULT NULL','MEDIUMINT(16) DEFAULT NULL',20,0,'mediumint',1316464299,0,0);
+insert  into `benchmark_fields` (`fid`,`name`,`type`,`required`,`primarykey`,`key`,`create_sql`,`alter_sql`,`size`,`recipricol`,`alter_type`,`created`,`updated`,`actioned`) values (8,'tinyiny','tinyint','_NO','_NO',NULL,'TINYINT(20) DEFAULT NULL','SMALLINT(14) DEFAULT NULL',20,0,'smallint',1316464299,0,0);
+insert  into `benchmark_fields` (`fid`,`name`,`type`,`required`,`primarykey`,`key`,`create_sql`,`alter_sql`,`size`,`recipricol`,`alter_type`,`created`,`updated`,`actioned`) values (9,'float','float','_NO','_NO',NULL,'REAL(15,5) DEFAULT NULL','DECIMAL(16,8) DEFAULT NULL',10,5,'double',1316464299,0,0);
+insert  into `benchmark_fields` (`fid`,`name`,`type`,`required`,`primarykey`,`key`,`create_sql`,`alter_sql`,`size`,`recipricol`,`alter_type`,`created`,`updated`,`actioned`) values (10,'decimal','decimal','_NO','_NO',NULL,'DECIMAL(15,5) DEFAULT NULL','REAL(16,8) DEFAULT NULL',10,5,'float',1316464299,0,0);
+insert  into `benchmark_fields` (`fid`,`name`,`type`,`required`,`primarykey`,`key`,`create_sql`,`alter_sql`,`size`,`recipricol`,`alter_type`,`created`,`updated`,`actioned`) values (11,'double','double','_NO','_NO',NULL,'DOUBLE(15,5) DEFAULT NULL','DECIMAL(16,8) DEFAULT NULL',10,5,'decimal',1316464299,0,0);
+insert  into `benchmark_fields` (`fid`,`name`,`type`,`required`,`primarykey`,`key`,`create_sql`,`alter_sql`,`size`,`recipricol`,`alter_type`,`created`,`updated`,`actioned`) values (12,'enum_int','enum_int','_NO','_NO',NULL,'ENUM(%numbersassymbols%) DEFAULT NULL','SMALLINT(5) DEFAULT NULL',2,0,'smallint',1316464299,0,0);
+insert  into `benchmark_fields` (`fid`,`name`,`type`,`required`,`primarykey`,`key`,`create_sql`,`alter_sql`,`size`,`recipricol`,`alter_type`,`created`,`updated`,`actioned`) values (13,'enum_varchar','enum_varchar','_NO','_NO',NULL,'ENUM(%numbersaswords%) DEFAULT NULL','VARCHAR(35) DEFAULT NULL',15,0,'varchar',1316464299,0,0);
+insert  into `benchmark_fields` (`fid`,`name`,`type`,`required`,`primarykey`,`key`,`create_sql`,`alter_sql`,`size`,`recipricol`,`alter_type`,`created`,`updated`,`actioned`) values (14,'mediumtext','mediumtext','_NO','_NO',NULL,'MEDIUMTEXT','VARCHAR(2000) DEFAULT NULL',2000,0,'varchar',1316464299,0,0);
+insert  into `benchmark_fields` (`fid`,`name`,`type`,`required`,`primarykey`,`key`,`create_sql`,`alter_sql`,`size`,`recipricol`,`alter_type`,`created`,`updated`,`actioned`) values (15,'text','text','_NO','_NO',NULL,'TEXT','VARCHAR(2000) DEFAULT NULL',2000,0,'varchar',1316464299,0,0);
+insert  into `benchmark_fields` (`fid`,`name`,`type`,`required`,`primarykey`,`key`,`create_sql`,`alter_sql`,`size`,`recipricol`,`alter_type`,`created`,`updated`,`actioned`) values (16,'date','date','_NO','_NO',NULL,'DATE DEFAULT NULL','VARCHAR(12) DEFAULT NULL',12,0,'varchar',1316464299,0,0);
+insert  into `benchmark_fields` (`fid`,`name`,`type`,`required`,`primarykey`,`key`,`create_sql`,`alter_sql`,`size`,`recipricol`,`alter_type`,`created`,`updated`,`actioned`) values (17,'time','time','_NO','_NO',NULL,'TIME DEFAULT NULL','VARCHAR(8) DEFAULT NULL',8,0,'varchar',1316464299,0,0);
+insert  into `benchmark_fields` (`fid`,`name`,`type`,`required`,`primarykey`,`key`,`create_sql`,`alter_sql`,`size`,`recipricol`,`alter_type`,`created`,`updated`,`actioned`) values (18,'datetime','datetime','_NO','_NO',NULL,'DATETIME DEFAULT NULL','VARCHAR(20) DEFAULT NULL',20,0,'varchar',1316464299,0,0);
+insert  into `benchmark_fields` (`fid`,`name`,`type`,`required`,`primarykey`,`key`,`create_sql`,`alter_sql`,`size`,`recipricol`,`alter_type`,`created`,`updated`,`actioned`) values (19,'varchar','varchar','_NO','_NO',NULL,'VARCHAR(255) DEFAULT NULL','MEDIUMTEXT',255,0,'mediumtext',1316464299,0,0);
+insert  into `benchmark_fields` (`fid`,`name`,`type`,`required`,`primarykey`,`key`,`create_sql`,`alter_sql`,`size`,`recipricol`,`alter_type`,`created`,`updated`,`actioned`) values (20,'created','int','_YES','_NO','made','INT(12) DEFAULT NULL','MEDIUMINT(12) DEFAULT NULL',12,0,'mediumint',1316464300,0,0);
+insert  into `benchmark_fields` (`fid`,`name`,`type`,`required`,`primarykey`,`key`,`create_sql`,`alter_sql`,`size`,`recipricol`,`alter_type`,`created`,`updated`,`actioned`) values (21,'year','year','_NO','_NO',NULL,'YEAR DEFAULT NULL','VARCHAR(4) DEFAULT NULL',4,0,'varchar',1316464300,0,0);
 
 CREATE TABLE `benchmark_results` (
   `rid` int(30) unsigned NOT NULL AUTO_INCREMENT,
@@ -55,13 +55,13 @@ CREATE TABLE `benchmark_results` (
   `difference` decimal(30,15) unsigned DEFAULT '0.000000000000000',
   `common_started` decimal(30,15) unsigned DEFAULT '0.000000000000000',
   `common_ended` decimal(30,15) unsigned DEFAULT '0.000000000000000',
-  `common_difference` decimal(30,15) unsigned DEFAULT '0.000000000000000',  
+  `common_difference` decimal(30,15) unsigned DEFAULT '0.000000000000000',
   `boot_started` decimal(30,15) unsigned DEFAULT '0.000000000000000',
   `boot_ended` decimal(30,15) unsigned DEFAULT '0.000000000000000',
   `boot_difference` decimal(30,15) unsigned DEFAULT '0.000000000000000',
   `created` int(13) unsigned DEFAULT '0',
-  `updated` int(13) unsigned DEFAULT '0',  
-  `actioned` int(13) unsigned DEFAULT '0',  
+  `updated` int(13) unsigned DEFAULT '0',
+  `actioned` int(13) unsigned DEFAULT '0',
   PRIMARY KEY (`rid`),
   KEY `common` (`rid`,`tid`,`test`,`number`,`tbid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -82,8 +82,8 @@ CREATE TABLE `benchmark_tables` (
   `truncated` int(12) DEFAULT '0',
   `dropped` int(12) DEFAULT '0',
   `created` int(13) unsigned DEFAULT '0',
-  `updated` int(13) unsigned DEFAULT '0',  
-  `actioned` int(13) unsigned DEFAULT '0',  
+  `updated` int(13) unsigned DEFAULT '0',
+  `actioned` int(13) unsigned DEFAULT '0',
   PRIMARY KEY (`tbid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -140,7 +140,7 @@ CREATE TABLE `benchmark_tests` (
   `min_rename_time` decimal(16,14) unsigned DEFAULT '0.00000000000000',
   `min_smarty_time` decimal(16,14) unsigned DEFAULT '0.00000000000000',
   `min_common_seconds` decimal(16,14) unsigned DEFAULT '0.00000000000000',
-  `min_boot_seconds` decimal(16,14) unsigned DEFAULT '0.00000000000000',  
+  `min_boot_seconds` decimal(16,14) unsigned DEFAULT '0.00000000000000',
   `sum_create_data_length` decimal(16,8) unsigned DEFAULT '0.00000000',
   `sum_select_data_length` decimal(16,8) unsigned DEFAULT '0.00000000',
   `sum_insert_data_length` decimal(16,8) unsigned DEFAULT '0.00000000',
@@ -150,7 +150,7 @@ CREATE TABLE `benchmark_tests` (
   `sum_deleteall_data_length` decimal(16,8) unsigned DEFAULT '0.00000000',
   `sum_alter_data_length` decimal(16,8) unsigned DEFAULT '0.00000000',
   `sum_rename_data_length` decimal(16,8) unsigned DEFAULT '0.00000000',
-  `sum_smarty_data_length` decimal(16,8) unsigned DEFAULT '0.00000000',  
+  `sum_smarty_data_length` decimal(16,8) unsigned DEFAULT '0.00000000',
   `min_create_data_length` decimal(16,8) unsigned DEFAULT '0.00000000',
   `min_select_data_length` decimal(16,8) unsigned DEFAULT '0.00000000',
   `min_insert_data_length` decimal(16,8) unsigned DEFAULT '0.00000000',
@@ -190,7 +190,7 @@ CREATE TABLE `benchmark_tests` (
   `sum_deleteall_fields` decimal(16,8) unsigned DEFAULT '0.00000000',
   `sum_alter_fields` decimal(16,8) unsigned DEFAULT '0.00000000',
   `sum_rename_fields` decimal(16,8) unsigned DEFAULT '0.00000000',
-  `sum_smarty_fields` decimal(16,8) unsigned DEFAULT '0.00000000',  
+  `sum_smarty_fields` decimal(16,8) unsigned DEFAULT '0.00000000',
   `min_create_fields` decimal(16,8) unsigned DEFAULT '0.00000000',
   `min_select_fields` decimal(16,8) unsigned DEFAULT '0.00000000',
   `min_insert_fields` decimal(16,8) unsigned DEFAULT '0.00000000',
@@ -230,7 +230,7 @@ CREATE TABLE `benchmark_tests` (
   `sum_deleteall_records` decimal(16,8) unsigned DEFAULT '0.00000000',
   `sum_alter_records` decimal(16,8) unsigned DEFAULT '0.00000000',
   `sum_rename_records` decimal(16,8) unsigned DEFAULT '0.00000000',
-  `sum_smarty_records` decimal(16,8) unsigned DEFAULT '0.00000000',  
+  `sum_smarty_records` decimal(16,8) unsigned DEFAULT '0.00000000',
   `min_create_records` decimal(16,8) unsigned DEFAULT '0.00000000',
   `min_select_records` decimal(16,8) unsigned DEFAULT '0.00000000',
   `min_insert_records` decimal(16,8) unsigned DEFAULT '0.00000000',
@@ -272,7 +272,7 @@ CREATE TABLE `benchmark_tests` (
   `took_rename_time` decimal(16,14) unsigned DEFAULT '0.00000000000000',
   `took_smarty_time` decimal(16,14) unsigned DEFAULT '0.00000000000000',
   `took_common_seconds` decimal(16,14) unsigned DEFAULT '0.00000000000000',
-  `took_boot_seconds` decimal(16,14) unsigned DEFAULT '0.00000000000000',  
+  `took_boot_seconds` decimal(16,14) unsigned DEFAULT '0.00000000000000',
   `tests_ran_create` int(10) unsigned DEFAULT '0',
   `tests_ran_select` int(10) unsigned DEFAULT '0',
   `tests_ran_insert` int(10) unsigned DEFAULT '0',
@@ -287,7 +287,7 @@ CREATE TABLE `benchmark_tests` (
   `started` int(13) unsigned DEFAULT '0',
   `ended` int(13) unsigned DEFAULT '0',
   `created` int(13) unsigned DEFAULT '0',
-  `updated` int(13) unsigned DEFAULT '0',  
+  `updated` int(13) unsigned DEFAULT '0',
   `actioned` int(13) unsigned DEFAULT '0',
   PRIMARY KEY (`tid`),
   KEY `common` (`stage`,`test`,`platform`)
